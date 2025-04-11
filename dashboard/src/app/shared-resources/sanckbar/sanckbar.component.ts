03d8a2de893b3d439f6a2bd600b2c4ff88288b3e
Abhi <apple@Apples-MacBook-Pro.local>
2025-04-11 09:22:10 +0530

    snackbar created

---
+++ dashboard/src/app/shared-resources/sanckbar/sanckbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sanckbar',
+  imports: [],
+  templateUrl: './sanckbar.component.html',
+  styleUrl: './sanckbar.component.scss'
+})
+export class SanckbarComponent {
+
+}
